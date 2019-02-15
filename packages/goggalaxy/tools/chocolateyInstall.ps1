406382adc63051bcb19b9d566abf326ddb800c85
Marc Würth <ravage@bluewin.ch>
2019-02-15 03:09:03 +0100

    Bump goggalaxy to 1.2.51.30

--- packages/goggalaxy/tools/chocolateyInstall.ps1
+++ packages/goggalaxy/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'goggalaxy' `
-    -Url 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.46.172.exe' `
-    -Checksum 'EDAE0A99386A56DADD858D08B97D6B2211F3361E498793BA44D573403BD1AFD0' `
+    -Url 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.51.30.exe' `
+    -Checksum '3C95A09F69D2B941B59FE65EB2E5B6DB2EFAAF6DDD502CF8943024AB421098AA' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/VERYSILENT /NORESTART'
